fc345dbd17c202867004e6f9f72b0463e529538f
Antonio <molineroromero@gmail.com>
2020-10-29 18:11:13 +0100

    markdown y array methods

---
+++ staff/antonio/js/array-methods/push.js
@@ -0,0 +1,8 @@
+function push(array, newItem) {
+    var = newItem
+    array = array(newItem)
+
+    return array.length
+}
+
+numeros = ["10", "11", "12"]
---
+++ staff/antonio/js/array-methods/some.js
@@ -0,0 +1,6 @@
+function some(array, even) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === even) console.log(true)
+        else console.log(false)
+    }
+}
