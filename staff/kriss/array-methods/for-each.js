d1c0e8f535fcf0ab85bde91295e8dfaa4785df83
krzysztof kowalski <krzysztofkowalski@krzysztofs-MacBook-Air.local>
2020-11-05 18:23:55 +0100

    array methods

---
+++ staff/kriss/array-methods/for-each.js
@@ -0,0 +1,7 @@
+
+boys.forEach(function (boy) {
+    console.log('Welcome ' + boy)
+})
+
+var allBoys = boys.toString()
+
---
+++ staff/kriss/array-methods/push.js
@@ -0,0 +1,10 @@
+function push (array) {
+array [array.length + 1]
+array == array.length + 1
+array = array.length + 1 
+
+
+}
+var numbers = [1, 2, 3, 4, 5 ] /* additonal element at the end of 
+an array */
+numbers.push (7);
