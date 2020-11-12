27cab40ca7731f734475104f0ed7b0d498a3ad86
Aitor Trujillo <lonesniper11@gmail.com>
2020-11-12 21:12:03 +0100

    boost dom & push login-register challenge up to getUser

--- staff/aitor-trujillo/login-register/index.js
+++ staff/aitor-trujillo/login-register/index.js
@@ -12,6 +12,7 @@ register.addEventListener('submit', function (event) {
     var surname = register['surname'].value
     var email = register['email-register'].value
     var password = register['password-register'].value
+
     try {
         registerUser(email, password, name, surname)
 
@@ -20,11 +21,46 @@ register.addEventListener('submit', function (event) {
 
         title.innerHTML = 'LOGIN'
     } catch (error) {
-        errorFeedback.innerHTML = error.message
+        errorFeedback.innerText = error.message
     }
 })
 
 login.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var email = login['email-login'].value
+    var password = login['password-login'].value
+
+    try {
+        var token = authenticateUser(email, password)
+
+        login.style.display = 'none'
+
+        // Get the user info (use getUser logic)
+
+        // Display the user info with append childs
+        title.innerText = 'Hello ' // + user.name
+
+        var div = document.createElement('div')
+        var ul = document.createElement('ul')
+        var li1 = document.createElement('li')
+        li1.innerText = 'Name: ' //+ user.name
+        var li2 = document.createElement('li')
+        li2.innerText = 'Surname: ' //+ user.surname
+        // email (new li with innerText)
+        ul.appendChild(li1)
+        ul.appendChild(li2)
+        // new ul.appendChild with email
+        div.appendChild(ul)
+        document.querySelector('body').appendChild(div)
+
+
+    } catch (error) {
+        var errorBanner = document.createElement('div')
+        errorBanner.innerText = error.message
+        errorBanner.className = 'error'
+        login.appendChild(errorBanner)
+    }
 
 
 })
