f5d9c522171abc56f4fde0930f83a7895acc1de8
krzysztof kowalski <krzysztofkowalski@krzysztofs-MacBook-Air.local>
2020-11-27 21:05:01 +0100

    array methods challenge

--- staff/kriss/array-methods/every.js
+++ staff/kriss/array-methods/every.js
@@ -1,3 +1,8 @@
-function everyElemrent (array) {
+function everyElement (array) {
     
+    let age = [18, 19, 20, 21 ]
+    function ageCheck () {
+        for  (var i = 0; i < array.length; i++)
+     if (age[i] < 18) console.log ("false")
+
 }
--- staff/kriss/auth-app/logic/edit-user.js
+++ staff/kriss/auth-app/logic/edit-user.js
@@ -1,21 +1,22 @@
-function editUser (address, gender, age, callback)  { // ?? What type of parametres do we need? How to edit user?
+function editUser (token, address, gender, age, callback)  { 
 
-    if (!ddress) throw TypeError ("full address is required")
-    if (! gernder) throw TypeError ("gender must be specified")
-    if (age < 18) throw TypeError ("You must be older than 18")
+    if (!address) throw TypeError ("full address is required")
+    if (!gender) throw TypeError ("gender must be specified")
+    if (!age) throw TypeError ("You must be older than 18")
 
-    const method = "POST"                    // not sure what method should I use (GET?)
+    const method = "PATCH"                    
     const url = "https://b00tc4mp.herokuapp.com/api/v2/users/ "
-    const headers = { "Content-Type": "application/json"}
+    const headers = { Authorization: `Bearer ${token}`, "Content-Type": "application/json"}
     const body =JSON.stringify ({address, gender, age })
+    
 
     call (method, url, headers,body, (status, res) => {
 
-        if (status ===201) {                        // the same not sure the number (Insomnia showing conflict)
-            const user = JSON.parse (res)
-             callback(user) 
+        if (status ===204) {                        
+            
+             callback() 
         } else {
-            const {error} = JSON.parse (error)
+            const {error} = JSON.parse (res)
             callback(error)
     
         }
