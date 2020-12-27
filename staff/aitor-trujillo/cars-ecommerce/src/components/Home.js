9a4dbddfc1a940ac6b5676a2f06a4a97c3d430b0
Aitor Trujillo <lonesniper11@gmail.com>
2020-12-27 10:59:16 +0100

    update nav

--- staff/aitor-trujillo/cars-ecommerce/src/components/Home.js
+++ staff/aitor-trujillo/cars-ecommerce/src/components/Home.js
@@ -1,54 +1,45 @@
-import { useEffect, useState } from 'react';
 import {
     withRouter,
     Route,
     Redirect,
     Switch
 } from "react-router-dom";
-
 import Nav from './Nav'
 import Search from './Search'
 import AllCars from './AllCars'
 import Profile from './Profile'
 import MyCars from './MyCars'
 
-function Home({ history, onLogout }) {
-    const [homeView, setHomeView] = useState('search')
+function Home({ history, location, onLogout }) {
 
     const handleSearchClick = (event) => {
         event.preventDefault()
-
-        setHomeView('search')
         history.push('/home/search')
     }
+
     const handleAllCarsClick = (event) => {
         event.preventDefault()
-
-        setHomeView('all-cars')
         history.push('/home/all-cars')
     }
+
     const handleProfileClick = (event) => {
         event.preventDefault()
-
-        setHomeView('profile')
         history.push('/home/profile')
     }
+
     const handleMyCarsClick = (event) => {
         event.preventDefault()
-
-        setHomeView('my-cars')
         history.push('/home/my-cars')
     }
 
     return (<>
         <Nav
-            selected={homeView}
+            location={location}
             searchClick={handleSearchClick}
             allCarsClick={handleAllCarsClick}
             profileClick={handleProfileClick}
             myCarsClick={handleMyCarsClick}
         />
-        {/* <Redirect to='/home/search' /> */}
         <Switch>
             <Route exact path='/home' render={() => <Redirect to='/home/search' />} />
             <Route path='/home/search' render={() => <Search />} />
--- staff/aitor-trujillo/cars-ecommerce/src/components/Nav.js
+++ staff/aitor-trujillo/cars-ecommerce/src/components/Nav.js
@@ -1,27 +1,18 @@
 import { useEffect, useState } from "react";
 
-function Nav({ selected, searchClick, allCarsClick, profileClick, myCarsClick }) {
+function Nav({ location, searchClick, allCarsClick, profileClick, myCarsClick }) {
     const [active, setActive] = useState('search')
 
     useEffect(() => {
-        setActive(selected)
-    }, [selected])
+        setActive(location.pathname.split('/')[2])
+    }, [location.pathname])
 
     return (
-        // TODO make check active inside className
         <nav className='nav'>
-            {active === 'search' ?
-                <a href='/' className='nav__item selected' onClick={searchClick}>Search</a> :
-                <a href='/' className='nav__item' onClick={searchClick}>Search</a>}
-            {active === 'all-cars' ?
-                <a href='/' className='nav__item selected' onClick={allCarsClick}>All Cars</a> :
-                <a href='/' className='nav__item' onClick={allCarsClick}>All Cars</a>}
-            {active === 'profile' ?
-                <a href='/' className='nav__item selected' onClick={profileClick}>Profile</a > :
-                <a href='/' className='nav__item' onClick={profileClick}>Profile</a >}
-            {active === 'my-cars' ?
-                <a href='/' className='nav__item selected' onClick={myCarsClick}>My Cars</a > :
-                <a href='/' className='nav__item' onClick={myCarsClick}>My Cars</a >}
+            <a href='/' className={`nav__item ${active === 'search' ? 'selected' : ''}`} onClick={searchClick}>Search</a>
+            <a href='/' className={`nav__item ${active === 'all-cars' ? 'selected' : ''}`} onClick={allCarsClick}>All Cars</a>
+            <a href='/' className={`nav__item ${active === 'profile' ? 'selected' : ''}`} onClick={profileClick}>Profile</a>
+            <a href='/' className={`nav__item ${active === 'my-cars' ? 'selected' : ''}`} onClick={myCarsClick}>My Cars</a>
         </nav >
     );
 }
